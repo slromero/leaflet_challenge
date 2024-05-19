cbb655cb9a1be5e259a89ef4c5836d43397e5e7c
slromero <slromero@optonline.net>
2024-05-18 20:28:02 -0400

    working files

--- Leaflet-Part-1/static/js/logic1.js
+++ Leaflet-Part-1/static/js/logic1.js
@@ -5,7 +5,7 @@ let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 d3.json(queryUrl).then(function (data) {
     // Once we get a response, send the data.features object to the createFeatures function.
     createFeatures(data.features);
-    // console.log("data.features: ", data.features)
+    console.log("data.features: ", data.features)
 });
 
 // Creating the map object
@@ -28,7 +28,7 @@ function markerColor(depth) {
 }
 
 function createFeatures(earthquakeData) {
-
+  let magnitude_array = [];
   // Loop through the earthquakes array, and create one marker for each earthquake object.
   for (let i = 0; i < earthquakeData.length; i++) {
     let earthquake = earthquakeData[i];
@@ -52,32 +52,4 @@ function createFeatures(earthquakeData) {
       console.error("Invalid magnitude or depth:", magnitude,depth);
     }
   }
-
-  // Set up the legend.
-  limits = [];
-  let legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    let colors = ["#2c7bb6","#00a6ca","#00ccbc","#90eb9d","#ffff8c","#f9d057","#f29e2e","#e76818","#d7191c"]
-    let labels = [];
-
-    // Add the minimum and maximum.
-    let legendInfo = "<h2>Earthquake Depth</h2>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + Math.min(limits) + "</div>" +
-        "<div class=\"max\">" + Math.max(limits) + "</div>" +
-      "</div>";
-
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
-
-  // Adding the legend to the map
-  legend.addTo(myMap);
 }
